aa803e0a2c3c0f7cc2b9825f178a2fb161d56a01
IFSP <IFSP@10.10.4.10>
2017-05-26 13:27:10 -0300

    Fiz a validação dos dados

--- application/models/Candidato_model.php
+++ application/models/Candidato_model.php
@@ -11,11 +11,11 @@
  *
  * @author Aluno
  */
-class Candidato_model extends MY_Model{
+class Candidato_model extends MY_Model {
+
     //put your code here
     protected $table = 'candidato';
     protected $pk = 'usuario_idusuario';
-    
     public $usuario_idusuario;
     public $nome_social;
     public $data_nascimento;
@@ -31,18 +31,44 @@ class Candidato_model extends MY_Model{
     public $disponibilidade_viajar;
     public $disponibilidade_mudar_residencia;
     public $outras_informacoes;
-    
+
     public function __construct() {
         parent::__construct();
     }
-    
-    public function get_candidato_by_token($token)
-    {
-        return  $this->db
-                ->from('usuario u')
-                ->join('candidato c', 'u.idusuario = c.usuario_idusuario')
-                ->where('u.token', $token)
-                ->get()
-                ->row(0, $this->model);
+
+    public function get_candidato_by_token($token) {
+        return $this->db
+                        ->from('usuario u')
+                        ->join('candidato c', 'u.idusuario = c.usuario_idusuario')
+                        ->where('u.token', $token)
+                        ->get()
+                        ->row(0, $this->model);
     }
+
+    public function validar_dados() {
+        $CI = & get_instance();
+        $CI->load->library('data_validator');
+        $validate = $CI->data_validator;
+
+        $validate->set('nome', $this->nome)->is_required()->min_length(5)->max_length(75)->is_alpha_num()
+                ->set('email', $this->email)->is_required()->is_email()
+                ->set('data_nascimento', $this->data_nascimento)->is_date()
+                ->set('sexo', $this->sexo)->max_length(20)
+                ->set('genero', $this->genero)->max_length(20)
+                ->set('estado_civil', $this->estado_civil)->max_length(20)
+                ->set('cpf', $this->cpf)->is_cpf()
+                ->set('portador_deficiencia', $this->portador_deficiencia)->max_length(3)
+                ->set('descricao_deficiencia', $this->descricao_deficiencia)->max_length(75)
+                ->set('foto', $this->foto)->max_length(150)
+                ->set('tipo_habilatacao', $this->tipo_habilitacao)->max_length(1)
+                ->set('veiculo_proprio', $this->veiculo_proprio)->max_length(75)
+                ->set('disponibilidade_viajar', $this->disponibilidade_viajar)->max_length(3)
+                ->set('disponibilidade_mudar_residencia', $this->disponibilidade_mudar_residencia)->max_length(3);
+
+        if ($validate->validate() === false) {
+            $this->erro = $validate->get_errors();
+            throw new Exception('Erro ao validar os dados');
+        }
+    }
+
 }
