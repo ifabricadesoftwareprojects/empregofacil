ed4c667c8a94db3983cbdfacf46c4d82a9e6fd19
IFSP <IFSP@LAB1PC03.labs.ifspcapivari.edu.br>
2017-06-21 16:01:05 -0300

    Modais Endereço e Contato

--- application/views/candidato/modais.php
+++ application/views/candidato/modais.php
@@ -50,7 +50,7 @@
                             <option value="Desistência"<?php echo (isset($dados_curso) && $dados_curso['status_curso'] == 'Desistência') ? ' selected' : '' ?>>Desistência</option>
                             <option value="Trancado"<?php echo (isset($dados_curso) && $dados_curso['status_curso'] == 'Trancado') ? ' selected' : '' ?>>Trancado</option>
                         </select>
-                        
+
                     </div>
                     <button type="submit" class="btn btn-primary ">Adicionar Curso</button>          
                 </form>
@@ -67,7 +67,7 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">Adicionar Experiência</h4>
-                
+
             </div>
             <div class="modal-body">
                 <form id="formExperiencia" method="post" action="<?php echo base_url('candidato/cadastrar_experiencia') ?>">
@@ -96,7 +96,7 @@
                         </select>                     
                         <?php echo display_erros(isset($erros['emprego_atual']) ? $erros['emprego_atual'] : null) ?>
                     </div>
-                    
+
                     <div class="form-group">
                         <label for="nivel_hierarquico">Nível hierarquico:</label>
                         <select name="nivel_hierarquico" class="form-control campoextra" campoextra="outro_nivel" compara="Outro">
@@ -125,7 +125,7 @@
                         <textarea class="form-control" rows="3" name="atividade_desempenhada" placeholder="Atividade que exerce discorra" ><?php echo (isset($dados_experiencia) ? $dados_experiencia['atividade_desempenhada'] : '') ?></textarea>
                         <?php echo display_erros(isset($erros['atividade_desempenhada']) ? $erros['atividade_desempenhada'] : null) ?>
                     </div>
-                     <button type="submit" class="btn btn-primary ">Adicionar Experiência</button>          
+                    <button type="submit" class="btn btn-primary ">Adicionar Experiência</button>          
                 </form>
             </div>
         </div><!-- /.modal-content -->
@@ -191,7 +191,7 @@
                         <label for="fala">Fala:</label></br>
 
                         <label class="radio-inline">
-                        <input type="radio" name="fala" value="Básico" />Básico             
+                            <input type="radio" name="fala" value="Básico" />Básico             
                         </label>
                         <label class="radio-inline">
                             <input type="radio" name="fala" value="Intermediário" />Intermédiario
@@ -215,7 +215,7 @@
                         </label>
                         <?php echo display_erros(isset($erros['le']) ? $erros['le'] : null) ?>
                     </div>
-                   
+
                     <button type="submit" class="btn btn-primary ">Adicionar Idioma</button>          
                 </form>
             </div>
@@ -242,12 +242,121 @@
                         <label for="foto">Carregar Nova Foto:</label>
                         <input type="file" name="foto" class="form-control">
                     </div>
-                     <button type="submit" class="btn btn-primary ">Atualizar Foto</button>
-                     <button type="button" class="btn" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary ">Atualizar Foto</button>
+                    <button type="button" class="btn" data-dismiss="modal">Cancelar</button>
                 </form>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-<!-- FIM MODAL DE EXPERIENCIA -->
+<!-- FIM MODAL DE FOTO -->
 
+<!-- MODAL DE ENDERECO -->       
+<div id="modalEndereco" class="modal fade" tabindex="-1" role="dialog" ref="formEndereco">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Adicionar Endereco</h4>
+
+            </div>
+            <div class="modal-body">
+                <form id="formExperiencia" method="post" action="<?php echo base_url('candidato/cadastrar_endereco') ?>">
+                    <input type="hidden" name="id" />
+                    <input type="hidden" name="acao" value="" />
+                    <div class="form-group">
+                        <label for="rua">Rua: </label>
+                        <input type="text" name="rua" class="form-control" placeholder="Nome da rua onde mora" value="<?php echo (isset($dados_endereco) ? $dados_endereco['rua'] : '') ?>">
+                        <?php echo display_erros(isset($erros['rua']) ? $erros['rua'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="numero">Numero: </label>
+                        <input type="number" name="numero" class="form-control" placeholder="333" value="<?php echo (isset($dados_endereco) ? $dados_endereco['numero'] : '') ?>">
+                        <?php echo display_erros(isset($erros['numero']) ? $erros['numero'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="complemento">Complemento: </label>
+                        <input type="text" name="complemento" class="form-control" placeholder="Complemento" value="<?php echo (isset($dados_endereco) ? $dados_endereco['complemento'] : '') ?>">
+                        <?php echo display_erros(isset($erros['complemento']) ? $erros['complemento'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="bairro">Bairro: </label>
+                        <input type="text" name="bairro" class="form-control" placeholder="Bairro" value="<?php echo (isset($dados_endereco) ? $dados_endereco['bairro'] : '') ?>">
+                        <?php echo display_erros(isset($erros['bairro']) ? $erros['bairro'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="cep">CEP:</label>
+                        <input type="text" class="form-control input-lg cpf"  placeholder="Ex: 13360-000" name="cep" value="<?php echo $candidato->cep ?>">
+                        <?php echo display_erros(isset($erros['cep']) ? $erros['cep'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="cidade">Cidade: </label>
+                        <input type="text" name="cidade" class="form-control" placeholder="Cidade" value="<?php echo (isset($dados_endereco) ? $dados_endereco['cidade'] : '') ?>">
+                        <?php echo display_erros(isset($erros['cidade']) ? $erros['cidade'] : null) ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="estado">Estado:</label>
+                        <select name="estado" class="form-control">
+                            <option value="estado">Selecione o Estado</option> 
+                            <option value="AC"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'AC') ? ' selected' : '' ?>>Acre</option> 
+                            <option value="AL"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'AL') ? ' selected' : '' ?>>Alagoas</option> 
+                            <option value="AM"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'AM') ? ' selected' : '' ?>>Amazonas</option> 
+                            <option value="AP"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'AP') ? ' selected' : '' ?>>Amapá</option> 
+                            <option value="BA"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'BA') ? ' selected' : '' ?>>Bahia</option> 
+                            <option value="CE"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'CE') ? ' selected' : '' ?>>Ceará</option> 
+                            <option value="DF"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'DF') ? ' selected' : '' ?>>Distrito Federal</option> 
+                            <option value="ES"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'ES') ? ' selected' : '' ?>>Espírito Santo</option> 
+                            <option value="GO"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'GO') ? ' selected' : '' ?>>Goiás</option> 
+                            <option value="MA"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'MA') ? ' selected' : '' ?>>Maranhão</option> 
+                            <option value="MT"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'MT') ? ' selected' : '' ?>>Mato Grosso</option> 
+                            <option value="MS"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'MS') ? ' selected' : '' ?>>Mato Grosso do Sul</option> 
+                            <option value="MG"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'MG') ? ' selected' : '' ?>>Minas Gerais</option> 
+                            <option value="PA"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'PA') ? ' selected' : '' ?>>Pará</option> 
+                            <option value="PB"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'PB') ? ' selected' : '' ?>>Paraíba</option> 
+                            <option value="PR"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'PR') ? ' selected' : '' ?>>Paraná</option> 
+                            <option value="PE"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'PE') ? ' selected' : '' ?>>Pernambuco</option> 
+                            <option value="PI"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'PI') ? ' selected' : '' ?>>Piauí</option> 
+                            <option value="RJ"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'RJ') ? ' selected' : '' ?>>Rio de Janeiro</option> 
+                            <option value="RN"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'RN') ? ' selected' : '' ?>>Rio Grande do Norte</option> 
+                            <option value="RO"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'RO') ? ' selected' : '' ?>>Rondônia</option> 
+                            <option value="RS"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'RS') ? ' selected' : '' ?>>Rio Grande do Sul</option> 
+                            <option value="RR"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'RR') ? ' selected' : '' ?>>Roraima</option> 
+                            <option value="SC"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'SC') ? ' selected' : '' ?>>Santa Catarina</option> 
+                            <option value="SE"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'SE') ? ' selected' : '' ?>>Sergipe</option> 
+                            <option value="SP"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'SP') ? ' selected' : '' ?>>São Paulo</option> 
+                            <option value="TO"<?php echo (isset($dados_endereco) && $dados_endereco['estado'] == 'TO') ? ' selected' : '' ?>>Tocantins</option> 
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary ">Adicionar Endereço</button>          
+                </form>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- FIM MODAL DE Endereco -->
+<!-- MODAL DE CONTATO -->       
+<div id="modalContato" class="modal fade" tabindex="-1" role="dialog" ref="formContato">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Adicionar Contato</h4>
+            </div>
+            <div class="modal-body">
+                <form id="formContato" method="post" action="<?php echo base_url('canditato/cadastrar_contato') ?>">
+                    <div class="form-group">
+                        <label for="tipo">Tipo:</label>
+                        <select name="tipo" class="form-control">
+                            <option value="tipo">Selecione o tipo</option> 
+                            <option value="telefone"<?php echo (isset($dados_contato) && $dados_contato['tipo'] == 'telefone') ? ' selected' : '' ?>>Telefone</option> 
+                            <option value="celular"<?php echo (isset($dados_contato) && $dados_contato['tipo'] == 'celular') ? ' selected' : '' ?>>Celular</option>
+                            <option value="email"<?php echo (isset($dados_contato) && $dados_contato['tipo'] == 'email') ? ' selected' : '' ?>>Email</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary ">Cadastrar Contato</button>            
+                </form>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- FIM MODAL DE CONTATO -->
