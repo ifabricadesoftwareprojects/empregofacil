a12c8630125eb554679e83e64ae9efb7ab38d4e3
IFSP <IFSP@10.134.74.112>
2017-10-20 19:05:28 -0200

    Retormar os novos campos da vaga quando editar

--- assets/js/empresa.js
+++ assets/js/empresa.js
@@ -23,9 +23,13 @@ $(document).ready(function () {
         $('#formVaga input[name="faixa_salarial_inicio"').val(dados.faixa_salarial_inicio);
         $('#formVaga input[name="faixa_salarial_fim"').val(dados.faixa_salarial_fim);
         $('#formVaga textarea[name="pre_requisitos"]').val(dados.pre_requisitos);
-        $('#formVaga input[name="tipo_contrato"').val(dados.tipo_contrato);
+        $('#formVaga select[name="tipo_contrato"').val(dados.tipo_contrato).change();
+        $('#formVaga input[name="numero_vagas"').val(dados.numero_vagas);
+        $('#formVaga select[name="acesso_vaga"').val(dados.acesso_vaga).change();
+        $('#formVaga select[name="exibir_dados"').val(dados.exibir_dados).change();
+        $('#formVaga input[name="vaga_pcd"').val(dados.vaga_pcd).change();
         $('#formVaga textarea[name="beneficios"]').val(dados.beneficios);
-        $('#formVaga input[name="status_vaga"').val(dados.status_vaga).change();
+        $('#formVaga select[name="status_vaga"').val(dados.status_vaga).change();
         $('#formVaga input[name="data_publicacao"').val(dados.data_publicacao);
         $('#formVaga input[name="visualizacoes"').val(dados.visualizacoes);
         $('#formVaga input[name="acao"]').val('editar');
